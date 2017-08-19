9c6f610fd1c5309d89e30e02e291b47f44cfd81a
Mark Bui <tvyybui@yahoo.com>
2017-08-19 12:56:36 -0700

    working on user.js

--- models/user.js
+++ models/user.js
@@ -1,1 +1,28 @@
-
+module.exports = function(sequelize, DataTypes) {
+  var user = sequelize.define("User", {
+    id: {
+      type: Sequelize.INTEGER(11).UNSIGNED,
+      autoIncrement: true,
+      primaryKey: true
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1, 50]
+      }
+    },
+    email: {
+      type: DataTypes.String,
+      isUnique: true,
+      allowNull: false,
+      validate: {
+        isEmail: {msg: 'Reason'}
+      }
+    },
+    password: {
+      
+    }
+  });
+  return User;
+};
