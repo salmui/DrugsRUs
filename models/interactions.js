1100755ca36aad547da3ab9497f406b4af7d2fb8
deffractedmind <albert.factora@gmail.com>
2017-08-25 07:05:01 -0700

    Fixed the model-table mapping

--- models/interactions.js
+++ models/interactions.js
@@ -1,10 +1,10 @@
 module.exports = function(sequelize, DataTypes) {
-    var Meds_Interactions = sequelize.define("Meds_Interactions", {
+    var Interactions = sequelize.define("Interactions", {
       med1_id: DataTypes.INTEGER,
       med2_id: DataTypes.INTEGER,
       med1_name: DataTypes.STRING(500),
       med2_name: DataTypes.STRING(500),
       interaction: DataTypes.STRING(2000)
     });
-    return Meds_Interactions;
+    return Interactions;
   };
